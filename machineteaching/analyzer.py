fc77f5aab021a50e8d68625ff46a729f63af753e
Laura Moraes <laura.moraes@twistsystems.com>
2019-04-04 20:48:53 -0300

    Fitting BKT

--- machineteaching/analyzer.py
+++ machineteaching/analyzer.py
@@ -14,56 +14,59 @@ def python_analyzer(doc):
     not_found = []
     vectorizer = CountVectorizer(**vectorizer_params)
     file = io.StringIO(doc)
-    for token in tokenize.generate_tokens(file.readline):
-        token_type = tokenize.tok_name[token[0]]
+    try:
+        for token in tokenize.generate_tokens(file.readline):
+            token_type = tokenize.tok_name[token[0]]
 
-        # Redundant conditional to make sure we're getting all the token types
-        if token_type not in removed_itens:
-            if token_type in allowed_itens:
-                # If it's a variable or reserved name, keep it
-                if token_type == "NAME":
-                    words.append(token[1])
-                elif token_type == "INDENT":
-                    # Adding indent for all indentations
-                    words.append("is_indent")
-                elif token_type == "DEDENT":
-                    # Adding dedent for all indentations
-                    words.append("is_dedent")
-                elif token_type == "STRING":
-                    # Adding is_string for every string
-                    words.append("is_string")
-                elif token_type == "NUMBER":
-                    # Adding is_number for every number:
-                    words.append("is_number")
-                elif token_type == "OP":
-                    # If it's operator, then we'll divide in several types
-                    lookup = {
-                        "+": "is_op_arit",
-                        "+=": "is_op_arit",
-                        "-": "is_op_arit",
-                        "*": "is_op_arit",
-                        "**": "is_op_arit",
-                        "/": "is_op_arit",
-                        "//": "is_op_arit",
-                        "%": "is_op_arit",
-                        ">": "is_op_logic",
-                        "<": "is_op_logic",
-                        ">=": "is_op_logic",
-                        "<=": "is_op_logic",
-                        "==": "is_op_logic",
-                        "-=": "is_op_logic",
-                        "!=": "is_op_logic",
-                        "[": "is_list",
-#                         "]": "is_list",
-                        "{": "is_dict",
-#                         "}": "is_dict",
-                        ".": "is_class",
-                        "=": "is_attribution",
-                        ":": "is_block"
-                    }
-                    try:
-                        words.append(lookup[token[1]])
-                    except KeyError:
-                        not_found.append(token[1])
-#     print("not found: %s" % set(not_found))
+            # Redundant conditional to make sure we're getting all the token types
+            if token_type not in removed_itens:
+                if token_type in allowed_itens:
+                    # If it's a variable or reserved name, keep it
+                    if token_type == "NAME":
+                        words.append(token[1])
+                    elif token_type == "INDENT":
+                        # Adding indent for all indentations
+                        words.append("is_indent")
+                    elif token_type == "DEDENT":
+                        # Adding dedent for all indentations
+                        words.append("is_dedent")
+                    elif token_type == "STRING":
+                        # Adding is_string for every string
+                        words.append("is_string")
+                    elif token_type == "NUMBER":
+                        # Adding is_number for every number:
+                        words.append("is_number")
+                    elif token_type == "OP":
+                        # If it's operator, then we'll divide in several types
+                        lookup = {
+                            "+": "is_op_arit",
+                            "+=": "is_op_arit",
+                            "-": "is_op_arit",
+                            "*": "is_op_arit",
+                            "**": "is_op_arit",
+                            "/": "is_op_arit",
+                            "//": "is_op_arit",
+                            "%": "is_op_arit",
+                            ">": "is_op_logic",
+                            "<": "is_op_logic",
+                            ">=": "is_op_logic",
+                            "<=": "is_op_logic",
+                            "==": "is_op_logic",
+                            "-=": "is_op_logic",
+                            "!=": "is_op_logic",
+                            "[": "is_list",
+    #                         "]": "is_list",
+                            "{": "is_dict",
+    #                         "}": "is_dict",
+                            ".": "is_class",
+                            "=": "is_attribution",
+                            ":": "is_block"
+                        }
+                        try:
+                            words.append(lookup[token[1]])
+                        except KeyError:
+                            not_found.append(token[1])
+    #     print("not found: %s" % set(not_found))
+    except IndentationError:
+        pass
     return vectorizer._word_ngrams(words)
---
+++ machineteaching/retrieve_model_and_vectorizer.py
@@ -0,0 +1,97 @@
+# DB
+from questions.models import Solution, Problem
+import psycopg2
+
+# Helpers
+# import numpy as np
+import pickle
+import base64
+
+# Preprocessing
+from tokenizer import create_bag_of_words
+from sklearn.feature_extraction.text import CountVectorizer
+
+
+class RetrieveModelAndVectorizer(object):
+    """ Recreate vectorizer and load saved model from DB """
+    def __init__(self):
+        self.connect()
+
+    def connect(self):
+        """ Connect to DB """
+        connection = psycopg2.connect(user="machineteaching",
+                                      password="***REMOVED***",
+                                      host="localhost",
+                                      database="machineteaching")
+        connection.autocommit = True
+        self.cursor = connection.cursor()
+
+    def get_original_solutions(self):
+        # Cleaning database
+        last_id = 132
+        problems = Problem.objects.filter(id__gt=last_id)
+        solutions_obj = Solution.objects.filter(
+            problem__in=problems).update(ignore=True)
+        print("Problems to be ignored: %d" % problems.count())
+
+        problems = Problem.objects.filter(id__lte=last_id)
+        # problems = Problem.objects.all()
+        print("Problems to be used: %d" % problems.count())
+
+        solutions_obj = Solution.objects.filter(
+            problem__in=problems, ignore=False).order_by('id')
+        # solutions_obj = Solution.objects.all().order_by('id')
+        print("Solutions to be used: %d" % solutions_obj.count())
+
+        # docs_id = []
+        # questions = []
+        solutions = []
+
+        # Fill separated structures
+        for sol in solutions_obj:
+            # docs_id.append(sol.id)
+            # questions.append(sol.problem.content)
+            solutions.append(sol.content)
+
+        print("Got %d documents" % (solutions_obj.count()))
+        return solutions
+
+    def get_where_items(self, exp_id):
+        """ Get where clause for experiment """
+        cols = ["vectorizer", "min_df", "is_binary", "distance",
+                "method", "dataset", "k", "model", "X"]
+        query = "SELECT %s from experiments_solution where experiment_id = %s" % (
+                ", ".join(cols), exp_id)
+        self.cursor.execute(query)
+        where_items = self.cursor.fetchall()
+        return where_items
+
+    def recreate_vectorizer(self, solutions, where_items, exp_id):
+        """ Recreate vectorizer from where clause conditions """
+        v = eval(where_items[0][0])
+        m = where_items[0][1]
+        b = where_items[0][2]
+        # dist = where_items[0][3]
+        # method = where_items[0][4]
+        # k = where_items[0][6]
+        # model_db = pickle.loads(base64.b64decode(where_items[0][7]))
+        # X = np.asarray(where_items[0][8])
+
+        train_data_features, vectorizer, feature_names = create_bag_of_words(
+            solutions, v, binary=b, min_df=m)
+
+        return vectorizer
+
+    def get_model_and_vectorizer(self, exp_id):
+        """ Recreate vectorizer and load saved model from database """
+        # Recreate vectorizer
+        where_items = self.get_where_items(exp_id)
+        solutions = self.get_original_solutions()
+        self.vectorizer = self.recreate_vectorizer(
+            solutions, where_items, exp_id)
+
+        # Load saved model
+        self.model_db = pickle.loads(base64.b64decode(where_items[0][7]))
+        self.model_db.n_jobs = 1
+
+        return self.vectorizer, self.model_db
