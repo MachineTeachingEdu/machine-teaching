97121a1a0238460cde00803119eb2ef01a880532
Laura Moraes <lmoraes@cos.ufrj.br>
2019-05-02 16:21:13 -0300

    Creating urls.py in evaluation app

--- machineteaching/evaluation/urls.py
+++ machineteaching/evaluation/urls.py
@@ -1,5 +1,8 @@
 from django.views.generic import CreateView
+from evaluation.models import PythonConcepts
 
 urlpatterns = [
-    url(r'^$',CreateView.as_view(model=YourModel, get_success_url=lambda: reverse('model_countries'),
+    path('', CreateView.as_view(model=PythonConcepts,
+         get_success_url=lambda: reverse('model_countries'),
             template_name='your_countries.html'), form_class=ChoiceForm, name='model_countries'),)
+]
