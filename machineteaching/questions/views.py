2eafdb2fb5aaa77e645ffd82396558fe7b20b520
Laura Moraes <laura.moraes@twistsystems.com>
2019-08-28 20:45:13 -0300

    Fixing permission to see student code

--- machineteaching/questions/views.py
+++ machineteaching/questions/views.py
@@ -35,7 +35,7 @@ def must_be_yours(model=None):
             # Must be yours or from your student
             is_professor = Professor.objects.filter(user=request.user)
             # Test if user is professor and if student is in his/her class
-            user_professor = is_professor and (obj.user.userprofile.user_class in is_professor)
+            user_professor = is_professor and (obj.user.userprofile.user_class in is_professor.prof_class.all())
 
             if not (obj.user == request.user) or not user_professor:
                 raise PermissionDenied
