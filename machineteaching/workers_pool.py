b3e9693d332aff40bed9026476748250f8116fb0
Laura Moraes (Piano) <lmoraes@cos.ufrj.br>
2020-01-13 11:33:18 -0200

    Testing PFA on Windows

---
+++ machineteaching/workers_pool.py
@@ -0,0 +1,19 @@
+import multiprocessing
+import time
+
+data = (
+    ['a', '2'], ['b', '4'], ['c', '6'], ['d', '8'],
+    ['e', '1'], ['f', '3'], ['g', '5'], ['h', '7']
+) 
+
+def mp_worker(data):
+    inputs, the_time = data
+    output = " Processs %s\tWaiting %s seconds" % (inputs, the_time)
+    time.sleep(int(the_time))
+    output += " Process %s\tDONE" % inputs
+    return output
+
+def mp_handler():
+    p = multiprocessing.Pool(2)
+    output = p.map(mp_worker, data)
+    return output
