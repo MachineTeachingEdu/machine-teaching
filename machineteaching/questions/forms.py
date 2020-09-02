d7c4423259f311449d04183e0df5563f06decad2
João Pedro Freire <joaopedrofreire@ufrj.br>
2020-09-02 19:08:58 -0300

    Tradução das páginas internas

--- machineteaching/questions/forms.py
+++ machineteaching/questions/forms.py
@@ -3,6 +3,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.utils.safestring import mark_safe
+from django.utils.translation import ugettext_lazy as _
 # from django.urls import reverse
 from questions.models import UserLog, Professor, OnlineClass, Chapter
 
@@ -12,16 +13,16 @@ class UserLogForm(ModelForm):
         exclude = ['timestamp', 'user', 'error_type']
 
 class SignUpForm(UserCreationForm):
-    PROGRAMMING = (("no", "No"),
-                   ("yes", "Yes"))
+    PROGRAMMING = (("no", _("No")),
+                   ("yes", _("Yes")))
     # PROFESSORS = Professor.objects.filter(active=True).values_list(
     #     "id", "user__first_name").order_by('user__first_name')
     # CLASSES = OnlineClass.objects.filter(active=True).values_list("id", "name")
     # professor = forms.ChoiceField(choices=PROFESSORS)
     # onlineclass = forms.ChoiceField(choices=CLASSES, label="Class")
-    class_code = forms.CharField()
-    programming = forms.ChoiceField(choices=PROGRAMMING, label="Did you have any programming experience before this course?")
-    accepted = forms.BooleanField(label=mark_safe('I accept the <a href="/terms_and_conditions">Terms and Conditions</a>'))
+    class_code = forms.CharField(label=_("Class code"))
+    programming = forms.ChoiceField(choices=PROGRAMMING, label=_(u"Did you have any programming experience before this course?"))
+    accepted = forms.BooleanField(label=mark_safe(_(u'I accept the <a href=/terms_and_conditions>Terms and Conditions</a>')))
 
     def __init__(self, *args, **kwargs):
         super(SignUpForm, self).__init__(*args, **kwargs)
@@ -36,18 +37,18 @@ class SignUpForm(UserCreationForm):
     def clean_email(self):
         email = self.cleaned_data.get('email')
         if email and User.objects.filter(email=email).exists():
-            raise forms.ValidationError(u'Email addresses must be unique.')
+            raise forms.ValidationError(_(u'Email addresses must be unique.'))
         return email
 
     def clean_class_code(self):
         class_code = self.cleaned_data.get('class_code')
         if not OnlineClass.objects.filter(class_code=class_code).exists():
-            raise forms.ValidationError(u'Invalid Class Code.')
+            raise forms.ValidationError(_(u'Invalid Class Code.'))
         return class_code
 
 class OutcomeForm(forms.Form):
-    onlineclass = forms.ModelChoiceField(queryset=OnlineClass.objects.all(), label="Class")
-    chapter = forms.ModelChoiceField(queryset=Chapter.objects.all(), label="Aula")
+    onlineclass = forms.ModelChoiceField(queryset=OnlineClass.objects.all(), label=_(u'Class'))
+    chapter = forms.ModelChoiceField(queryset=Chapter.objects.all(), label=_(u'Chapter'))
 
     def __init__(self, *args, **kwargs):
         self.user = kwargs.pop('user', None)
@@ -56,5 +57,5 @@ class OutcomeForm(forms.Form):
     def clean_onlineclass(self):
         onlineclass = self.cleaned_data.get('onlineclass')
         if onlineclass not in OnlineClass.objects.filter(professor__user=self.user):
-            raise forms.ValidationError(u'You don\t have authorization to view this class.')
+            raise forms.ValidationError(_(u'You don\'t have authorization to view this class.'))
         return onlineclass
