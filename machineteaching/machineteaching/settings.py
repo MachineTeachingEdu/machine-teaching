9371f14a3106ef37b4c7acd3709b66700e4ba479
gxara <gabrielxara@gmail.com>
2022-04-03 10:45:08 -0300

    Add DB_ENGINE as env variable

--- machineteaching/machineteaching/settings.py
+++ machineteaching/machineteaching/settings.py
@@ -96,7 +96,7 @@ WSGI_APPLICATION = 'machineteaching.wsgi.application'
 # }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': os.getenv("DB_ENGINE", 'django.db.backends.postgresql_psycopg2'),
         'NAME': os.getenv("DB_NAME"),
         'USER': os.getenv("DB_USER"),
         'PASSWORD': os.getenv("DB_PASSWORD"),
