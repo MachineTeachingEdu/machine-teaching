2c0b3d1a91a81f33441ff71f0a2d195bc06eccf0
João Pedro Freire <joaopedrofreire@ufrj.br>
2022-05-04 22:12:13 -0300

    Class results script

--- machineteaching/questions/class_analysis.py
+++ machineteaching/questions/class_analysis.py
@@ -24,17 +24,14 @@ def count_on_time_exercises(user, chapters, onlineclass):
 
 
 
+f = open("questions/class_analysis/results.csv","w+")
+writer = csv.writer(f)
+writer.writerow(['turma','aluno','previsao','exercicios_resolvidos'])
+
 classes = [119,102,103,97,99,100,101,104,109,110,98,112,113,115,117]
 
 for id in classes:
     onlineclass = OnlineClass.objects.get(pk=id)
-    professor = onlineclass.professor.first().user
-
-    f = open("questions/class_analysis/results.csv","w+")
-    writer = csv.writer(f)
-
-    writer.writerow(['turma','aluno','previsao','exercicios_resolvidos'])
-
     professors = Professor.objects.all().values_list('user')
     students = User.objects.filter(userprofile__user_class=onlineclass).exclude(pk__in=professors)
 
