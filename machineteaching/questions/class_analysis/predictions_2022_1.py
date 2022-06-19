4c92830c9c79f9438a912d5b2729d4aa4857bb2e
João Pedro Freire <joaopedrofreire@ufrj.br>
2022-06-19 18:30:41 -0300

    Update class analysis scripts

--- machineteaching/questions/class_analysis/predictions_2022_1.py
+++ machineteaching/questions/class_analysis/predictions_2022_1.py
@@ -12,7 +12,7 @@ for id in classes:
     students = User.objects.filter(userprofile__user_class=onlineclass).exclude(pk__in=professors)
         
     f = open("questions/class_analysis/"+semester+"/"+professor.first_name+" - "+onlineclass.name+".txt","w+")
-    f.write(onlineclass.name.upper()+'\nProfessor: '+professor.first_name=' '+professor.last_name+' ('+professor.email+')\n\n')
+    f.write(onlineclass.name.upper()+'\nProfessor: '+professor.first_name+' '+professor.last_name+' ('+professor.email+')\n\n')
     f.write('Através da análise de dados da ferramenta Machine Teaching, foi identificado que os seguintes alunos estão em risco de abandono da disciplina Comp1:\n\n')
 
     i=1
