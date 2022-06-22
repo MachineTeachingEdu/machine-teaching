7ce9d63235c1f7027834ecefee79edcb76b2c6c3
João Pedro Freire <joaopedrofreire@ufrj.br>
2022-06-22 18:36:21 -0300

    Update class predictions script

--- machineteaching/questions/class_analysis/predictions_2022_1.py
+++ machineteaching/questions/class_analysis/predictions_2022_1.py
@@ -10,24 +10,25 @@ for id in classes:
     onlineclass = OnlineClass.objects.get(pk=id)
     professors = Professor.objects.all().values_list('user')
     students = User.objects.filter(userprofile__user_class=onlineclass).exclude(pk__in=professors)
-    professor = onlineclass.professor.exclude(pk__in=[319,344]).exclude(assistant=True).last().user
-        
-    f = open("questions/class_analysis/"+semester+"/"+professor.first_name+" - "+onlineclass.name.replace("/","_")+".txt","w+")
-    f.write(onlineclass.name.upper()+'\nProfessor: '+professor.first_name+' '+professor.last_name+' ('+professor.email+')\n\n')
-    f.write('Através da análise de dados da ferramenta Machine Teaching, foi identificado que os seguintes alunos estão em risco de abandono da disciplina Comp1:\n\n')
+    if onlineclass.professor.count():
+        professor = onlineclass.professor.exclude(pk__in=[319,344]).exclude(assistant=True).last().user
+            
+        f = open("questions/class_analysis/"+semester+"/"+professor.first_name+" - "+onlineclass.name.replace("/","_")+".txt","w+")
+        f.write(onlineclass.name.upper()+'\nProfessor: '+professor.first_name+' '+professor.last_name+' ('+professor.email+')\n\n')
+        f.write('Através da análise de dados da ferramenta Machine Teaching, foi identificado que os seguintes alunos estão em risco de abandono da disciplina Comp1:\n\n')
 
-    i=1
-    for student in students:
-        predict = predict_drop_out(student.id, onlineclass, date)
-        
-        try:
-            if predict < 7:
-                try:
-                    last_log = "último exercício resolvido em "+UserLog.objects.filter(user=student).order_by('timestamp').last().timestamp.strftime("%m/%d/%Y")
-                except:
-                    last_log = "nenhum exercício resolvido"
-                f.write(str(i)+'. '+student.first_name+' '+student.last_name+': '+last_log+' ('+student.email+')\n')
-                i+=1
-        except:
-            pass
-    f.close()
+        i=1
+        for student in students:
+            predict = predict_drop_out(student.id, onlineclass, date)
+            
+            try:
+                if predict < 7:
+                    try:
+                        last_log = "último exercício resolvido em "+UserLog.objects.filter(user=student).order_by('timestamp').last().timestamp.strftime("%m/%d/%Y")
+                    except:
+                        last_log = "nenhum exercício resolvido"
+                    f.write(str(i)+'. '+student.first_name+' '+student.last_name+': '+last_log+' ('+student.email+')\n')
+                    i+=1
+            except:
+                pass
+        f.close()
