507c651ae75e383d8df88408d4e0530754a47040
gxara <gabrielxara@gmail.com>
2022-07-07 23:11:58 -0300

    add load test configuration file and pulled last migrations

---
+++ load-test/mocks/mocked_solution.py
@@ -0,0 +1,23 @@
+solution = {
+    'problem': '802',
+    'solution': "def PosNegZero(x):\n\tif x>0:\n\t\treturn str(x) + \" e positivo\"\n\telif x<0:\n\t\treturn str(x) + \" e negativo\"\n\telse:\n\t\treturn str(x) + \" e zero\"",
+    'outcome': 'P',
+    'seconds_in_code': '68',
+    'seconds_to_begin': '80',
+    'seconds_in_page': '198',
+    'solution_lines': '10',
+    'console': [
+        "-43 e negativo","-68 e negativo","27 e positivo","77 e positivo","16 e positivo\n",
+        "-75 e negativo","60 e positivo","25 e positivo","31 e positivo","20 e positivo\n",
+        "-51 e negativo","4 e positivo","26 e positivo","-3 e negativo","-20 e negativo\n",
+        "-39 e negativo","0 e zero","25 e positivo","-73 e negativo","-79 e negativo\n",
+        "-70 e negativo","55 e positivo","3 e positivo","74 e positivo","54 e positivo\n",
+        "-23 e negativo","-24 e negativo","-7 e negativo","57 e positivo","-41 e negativo\n",
+        "15 e positivo","58 e positivo","-65 e negativo","-14 e negativo","49 e positivo\n",
+        "0 e zero","78 e positivo","57 e positivo","8 e positivo","-9 e negativo","-69 e negativo\n",
+        "-40 e negativo","-11 e negativo","-20 e negativo","-28 e negativo","79 e positivo\n",
+        "72 e positivo","-38 e negativo","-76 e negativo","-50 e negativo","9 e positivo",
+    ]
+}
+
+
