23fade6c789310a7ce093f2e2171899aad9ed791
João Pedro Freire <joaopedrofreire@ufrj.br>
2022-08-08 19:49:43 -0300

    Update drop out models evaluation

--- machineteaching/questions/get_dashboards.py
+++ machineteaching/questions/get_dashboards.py
@@ -164,8 +164,7 @@ def predict_drop_out(user, onlineclass, date):
     model = completed_chapter.drop_out_model
     chapters = model.completed_chapter.all()
     # Open model file
-    with open(model.model_file, "rb") as pklfile:
-        model = pickle.load(pklfile)
+    model=pd.read_pickle(model.model_file)
 
     X = get_on_time_exercises(user, chapters, onlineclass)
     X = sm.add_constant(X, has_constant='add')
