dace0cc0fadd4c13b4aea8a043eaadec42f2ce13
gxara <gabrielxara@gmail.com>
2022-09-02 09:56:43 -0300

    fix: debug env var

--- machineteaching/machineteaching/settings.py
+++ machineteaching/machineteaching/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv("APP_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.getenv("DEBUG")) == False
+DEBUG = bool(os.getenv("DEBUG"))
 
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split(",")
 
