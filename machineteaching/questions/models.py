a7366460277c796d2149ad3d54d51373836a9baa
hugofolloni <hugofollogua07@gmail.com>
2022-10-05 00:19:11 -0300

    Adding restriction to professor model

--- machineteaching/questions/models.py
+++ machineteaching/questions/models.py
@@ -126,7 +126,7 @@ class Deadline(models.Model):
 
 
 class Professor(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, unique=True)
     prof_class = models.ManyToManyField(OnlineClass, related_name='professor')
     assistant = models.BooleanField(default=False)
     active = models.BooleanField(default=True)
