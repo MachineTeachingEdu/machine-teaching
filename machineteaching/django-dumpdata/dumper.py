b3eba40a1e28edc02fc59a05ab05e017d3e50e7a
RonAlffrson <renan.c.alves@edu.unirio.br>
2023-10-03 14:28:27 -0300

    DonwloadDB app and dumper

--- machineteaching/django-dumpdata/dumper.py
+++ machineteaching/django-dumpdata/dumper.py
@@ -7,7 +7,7 @@ date = datetime.datetime.now()
 
 def dumping():
     print("Dumping data at", date)
-    cmd = "/usr/bin/python3 /home/renan/MachineTeaching/machine-teaching/machineteaching/manage.py dumpdata --exclude=auth.user --exclude=questions.userprofile --indent=2 > django_data.json"
+    cmd = "/home/renancalves/py-venv/bin/python3 /home/renancalves/MachineTeaching/machine-teaching/machineteaching/manage.py dumpdata --exclude=auth.user --exclude=questions.userprofile --indent=2 > django_data.json"
     os.system(cmd)
     os.system("mv django_data.json django-dumpdata")
 
--- machineteaching/downloadDB/views.py
+++ machineteaching/downloadDB/views.py
@@ -1,36 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseRedirect
-from .forms import NameForm, EmailForm, InstitutionForm, PretendedUseForm, AgreeForm
+from .forms import DownloadDumpForm
+from django.core.mail import send_mail
  
 # Create your views here. 
 def index(request):
-  return render(request, 'downloadDB/index.html')
-
-
-def get(request):
     # if this is a POST request we need to process the form data
     if request.method == "POST":
         # create a form instance and populate it with data from the request:
-        name_form = NameForm(request.POST)
-        email_form = EmailForm(request.POST)
-        institution_form = InstitutionForm(request.POST)
-        pretended_use_form = PretendedUseForm(request.POST)
-        agree_form = AgreeForm(request.POST)
+        form = DownloadDumpForm(request.POST)
         # check whether it's valid:
-        if form.is_valid() and email_form.is_valid() and institution_form.is_valid() and pretended_use_form.is_valid() and agree_form.is_valid():
+        if form.is_valid():
+            name = form.cleaned_data.get('name')
+            #TODO:
+            email = form.cleaned_data.get('email').lower()
+            institution = form.cleaned_data.get('institution')
+            pretended_use = form.cleaned_data.get('pretended_use')
+            message_subject = 'Requisição de dados'
+            message_content = 'Nome: {} \nE-mail: {} \nInstituição: {} \n\nUso pretendido: {}'.format(name, email, institution, pretended_use)
+            message_html = """
+            <div style='display: grid; grid-auto-flow: row; padding: 20px; width: 400px; height: 300px; background-color: #ffffff; border-radius: 10px;'>
+              <h4 style='color: #292929; grid-column: 1;'> Requisição de dados.</h4> 
+              <p style='color: #292929; margin-left: 20px; font-weight: 300; grid-column: 2;'> <strong>Nome:</strong> {} </p> 
+              <p style='color: #292929; margin-left: 20px; font-weight: 300; grid-column: 3;'> <strong>Instituição:</strong> {}</p><br>
+              <p style='color: #292929; margin-left: 20px; font-weight: 300; grid-column: 4;'> <strong>Uso pretendido:</strong> {}</p>
+            </div>""".format(name, institution, pretended_use)
+            
+            send_mail(message_subject, message_content, None, [email], fail_silently=False, html_message=message_html)
+
+
+
             # process the data in form.cleaned_data as required
+            
+            # Recuperar os valores do form utilizando form.cleaned_data.get('VARIAVEL')
+            # Cria o content e html do email com essas variaveis
+            # send_email(....)
+
             # ...
             # redirect to a new URL:
-            return HttpResponseRedirect("/thanks/")#TODO - thanks page
+            return HttpResponseRedirect("/thanks/") #TODO maybe return to start idk
 
     # if a GET (or any other method) we'll create a blank form
     else:
-        form = NameForm()
-        email_form = EmailForm()
-        institution_form = InstitutionForm()
-        pretended_use_form = PretendedUseForm()
-        agree_form = AgreeForm()
+        form = DownloadDumpForm() 
 
-    return render(request, "index.html", {"name_form": name_form, "email_form" : email_form,
-                                          "institution_form" : institution_form, "pretended_use_form" : pretended_use_form})
-
+    return render(request, "downloadDB/index.html", {"form": form})
