fb7e9a2045c8ba88d6f81b5bba24eb4f7ac374ed
RonAlffrson <renan.c.alves@edu.unirio.br>
2023-11-30 18:59:02 -0300

    thanks page

--- machineteaching/downloadDB/urls.py
+++ machineteaching/downloadDB/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from downloadDB import views
+from django.views.generic import TemplateView
 
 
 from . import views
 urlpatterns=[
-    path('', views.index, name='downloadDB_index')
-]
+    path('', views.index, name='downloadDB_index'),
+    path('thanks', TemplateView.as_view(template_name='downloadDB/thanks.html'), name='thanks')
+]   
